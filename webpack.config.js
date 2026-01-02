9967da616e430651249b5e6fe1a5cf4c69c5d10d
Ланских Михаил <lanskikh-mikhail@mail.ru>
2026-01-02 19:02:11 +0500

    module1-task2 - Начинаем программировать

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    filename: "bundle.[contenthash].js",
+    path: path.resolve(__dirname, "build"),
+    clean: true,
+  },
+  devtool: "source-map",
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "public",
+          globOptions: {
+            ignore: ["**/index.html"],
+          },
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
